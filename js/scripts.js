29baee3e995453e86713388267aa796f86afcac0
Niethilley Braz <niethilley@gmail.com>
2025-10-28 14:54:49 -0300

    OK

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-console.log("Aopa")
+const generatePasswordButton = document.querySelector("#generate-password")
+const gerenatedPasswordElement = document.querySelector("#generated-password")
+
+generatePasswordButton.addEventListener("click", ()=>{
+    generatePassword(getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol); 
+})
+
+const getLetterLowerCase = ()=>{
+    return(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+}
+const getLetterUpperCase = ()=>{
+    return(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
+}
+const getNumber = ()=>{
+    return Math.floor(Math.random()*10).toString()
+}
+const getSymbol = () =>{
+    const symbols = "(){}[]!@#$%^&*?.,;:><"
+    return symbols[Math.floor(Math.random()*symbols.length)].toString()
+}
+
+const generatePassword = (getLetterLowerCase,getLetterUpperCase,getNumber,getSymbol) => {
+    let password = ""
+    const passwordLenth = 10
+    
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol,
+    ]
+    
+    for (let i = 0; i < passwordLenth; i++) {
+        const randomValue =
+        generators[Math.floor(Math.random() * generators.length)]();
+        password += randomValue;
+    }
+
+    gerenatedPasswordElement.style.display="block"
+    gerenatedPasswordElement.querySelector("h4").innerText = password
+}
